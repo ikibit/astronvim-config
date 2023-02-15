99bb9e21c3397bb3e23533e0b577136571475b10
Barbaros <77664141+ikibit@users.noreply.github.com>
2023-02-15 15:55:33 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -24,7 +24,7 @@ local config = {
         -- },
     },
     -- Set colorscheme to use
-    colorscheme = "tokyonight",
+    colorscheme = "catppuccin",
     -- Add highlight groups in any theme
     highlights = {
         -- init = { -- this table overrides highlights in all themes
@@ -183,13 +183,13 @@ local config = {
             --   },
             -- },
             elixirls = {
-                cmd = { "/home/barbaros/.local/share/nvim/mason/packages/elixir-ls/language_server.sh" }, -- Linux
-                -- cmd = { "C:/Users/Barbaros/AppData/Local/nvim-data/mason/packages/elixir-ls/language_server.bat" }, -- Windows
+                -- cmd = { "/home/barbaros/.local/share/nvim/mason/packages/elixir-ls/language_server.sh" }, -- Linux
+                cmd = { "C:/Users/Barbaros/AppData/Local/nvim-data/mason/packages/elixir-ls/language_server.bat" }, -- Windows
                 -- cmd = { "/Users/barbaroskurt/.local/share/nvim/mason/packages/elixir-ls/language_server.sh" }, -- macOS
             },
             omnisharp = {
-                cmd = { "/home/barbaros/.local/share/nvim/mason/packages/omnisharp/OmniSharp" }, -- Linux
-                -- cmd = { "C:/Users/Barbaros/AppData/Local/nvim-data/mason/packages/omnisharp/OmniSharp" }, -- Windows
+                -- cmd = { "/home/barbaros/.local/share/nvim/mason/packages/omnisharp/OmniSharp" }, -- Linux
+                cmd = { "C:/Users/Barbaros/AppData/Local/nvim-data/mason/packages/omnisharp/OmniSharp" }, -- Windows
                 -- cmd = { "/Users/barbaroskurt/.local/share/nvim/mason/packages/omnisharp/OmniSharp" }, --macOS
             },
         },
@@ -220,42 +220,51 @@ local config = {
     plugins = {
         init = {
             {
-                "folke/tokyonight.nvim",
+                "catppuccin/nvim",
                 config = function()
-                    require("tokyonight").setup({
-                        -- your configuration comes here
-                        -- or leave it empty to use the default settings
-                        style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                        light_style = "day", -- The theme is used when the background is set to light
-                        transparent = false, -- Enable this to disable setting the background color
-                        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-                        styles = {
-                            -- Style to be applied to different syntax groups
-                            -- Value is any valid attr-list value for `:help nvim_set_hl`
-                            comments = { italic = true },
-                            keywords = { italic = true },
-                            functions = {},
-                            variables = {},
-                            -- Background styles. Can be "dark", "transparent" or "normal"
-                            sidebars = "dark", -- style for sidebars, see below
-                            floats = "dark", -- style for floating windows
+                    require("catppuccin").setup({
+                        flavour = "macchiato", -- latte, frappe, macchiato, mocha
+                        background = { -- :h background
+                            light = "latte",
+                            dark = "macchiato",
                         },
-                        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-                        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-                        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-                        dim_inactive = false, -- dims inactive windows
-                        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-                        --- You can override specific color groups to use other groups or a hex color
-                        --- function will be called with a ColorScheme table
-                        ---@param colors ColorScheme
-                        on_colors = function(colors)
-                        end,
-                        --- You can override specific highlights to use other groups or a hex color
-                        --- function will be called with a Highlights and ColorScheme table
-                        ---@param highlights Highlights
-                        ---@param colors ColorScheme
-                        on_highlights = function(highlights, colors)
-                        end,
+                        transparent_background = true,
+                        show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+                        term_colors = false,
+                        dim_inactive = {
+                            enabled = false,
+                            shade = "dark",
+                            percentage = 0.15,
+                        },
+                        no_italic = false, -- Force no italic
+                        no_bold = false, -- Force no bold
+                        styles = {
+                            comments = { "italic" },
+                            conditionals = { "italic" },
+                            loops = {},
+                            functions = {},
+                            keywords = {},
+                            strings = {},
+                            variables = {},
+                            numbers = {},
+                            booleans = {},
+                            properties = {},
+                            types = {},
+                            operators = {},
+                        },
+                        color_overrides = {},
+                        custom_highlights = {},
+                        integrations = {
+                            cmp = true,
+                            gitsigns = true,
+                            telescope = true,
+                            mason = true,
+                            neotree = true,
+                            treesitter = true,
+                            which_key = true,
+                            notify = true,
+                            -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+                        },
                     })
                 end,
             }
@@ -311,6 +320,11 @@ local config = {
         },
         ["mason-nvim-dap"] = { -- overrides `require("mason-nvim-dap").setup(...)`
             -- ensure_installed = { "python" },
+        },
+        ["neo-tree"] = { -- overrides `require("mason-nvim-dap").setup(...)`
+            source_selector = {
+                winbar = false,
+            },
         },
     },
     -- LuaSnip Options
